08aa3f45e48798b5d81cc3ffa0e87d775e1ffbbd
Alen Siljak <alen.siljak@gmail.com>
2012-07-18 16:09:29 +0200

    creating the form

--- src/SkypeAPIClient/Form1.Designer.cs
+++ src/SkypeAPIClient/Form1.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter the user and the message text, then click Send to send message through Skyp" +
+    "e.";
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(144, 62);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(255, 20);
+            this.UsernameTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message Text:";
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(144, 104);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MessageTextBox.Size = new System.Drawing.Size(255, 91);
+            this.MessageTextBox.TabIndex = 5;
             // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 339);
+            this.ClientSize = new System.Drawing.Size(491, 280);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Skype Message Send";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox MessageTextBox;
     }
 }
 
--- src/SkypeAPIClient/Form1.cs
+++ src/SkypeAPIClient/Form1.cs
@@ -15,5 +15,39 @@ namespace SkypeAPIClient
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sanitize_input();
+                send_message(UsernameTextBox.Text, MessageTextBox.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                // throw;
+            }
+        }
+
+        private void sanitize_input()
+        {
+            if (string.IsNullOrWhiteSpace(UsernameTextBox.Text))
+            {
+                throw new ArgumentException("Username can not be blank.");
+            }
+            UsernameTextBox.Text = UsernameTextBox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(MessageTextBox.Text))
+            {
+                throw new ArgumentException("Message should not be blank.");
+            }
+            MessageTextBox.Text = MessageTextBox.Text.Trim();
+        }
+
+        private void send_message(string username, string message)
+        {
+            
+        }
     }
 }
